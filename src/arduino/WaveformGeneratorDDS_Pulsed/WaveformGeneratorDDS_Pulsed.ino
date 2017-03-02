635a68860b0920e37d4eec6a9697ffacea899ddc
Jimbles <j.avery.10@ucl.ac.uk>
2017-03-02 11:04:16 +0000

    Pulsed Wave every Second

--- src/arduino/WaveformGeneratorDDS_Pulsed/WaveformGeneratorDDS_Pulsed.ino
+++ src/arduino/WaveformGeneratorDDS_Pulsed/WaveformGeneratorDDS_Pulsed.ino
@@ -2,6 +2,7 @@
 
 DDS Waveform generator for Arduino Due. Uses interupts to cycle through Wavetable.
 
+Wave every second through another timer
 
 */
 
@@ -18,32 +19,40 @@ void setup() {
 	pinMode(41, OUTPUT);
 	digitalWriteDirect(41, 0);
 
-	delay(1);
+	//	delay(1);
 
 	pmc_set_writeprotect(false);		 // disable write protection for pmc registers
 	pmc_enable_periph_clk(ID_TC3);	 // enable peripheral clock TC3 this means T3 on TC1 channel 0  - this is the timer for the stim trigger output
-	// set up timers and interupts - set channel on timers, set to "wave mode" meaning an output rather than "capture" to read ticks
-	TC_Configure(TC1, 0, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK1); // use TC1 channel 0 in "count up mode" using MCLK /2 clock1 to give 42MHz
+	pmc_enable_periph_clk(ID_TC1);	 // enable peripheral clock TC1 this means T3 on TC0 channel 1  - this is the timer for the stim trigger output
 
-	TC_SetRC(TC1, 0,84); // count 84 ticks on the 42MHz clock before calling the overflow routine - this gives an interupt every 2 uS
-	// enable timer interrupts on the timer for stim tigger output
+	 // set up timers and interupts - set channel on timers, set to "wave mode" meaning an output rather than "capture" to read ticks
+	TC_Configure(TC1, 0, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK1); // use TC1 channel 0 in "count up mode" using MCLK /2 clock1 to give 42MHz
+	TC_Configure(TC0, 1, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK4); // use TC0 channel 1 in "count up mode" using MCLK /128 clock1 to give 65.625 kHz
+
+
+	TC_SetRC(TC1, 0, 84); // count 84 ticks on the 42MHz clock before calling the overflow routine - this gives an interupt every 2 us
+	TC_SetRC(TC0, 1, 656250); // count 656250 ticks on the 65.625 kHz clock before calling the overflow routine - this gives an interupt every 1 s
+
+
+	// enable timer interrupts on the timer for DAC and Pulse timers
 	TC1->TC_CHANNEL[0].TC_IER = TC_IER_CPCS;   // IER = interrupt enable register
 	TC1->TC_CHANNEL[0].TC_IDR = ~TC_IER_CPCS;  // IDR = interrupt disable register
+	TC0->TC_CHANNEL[1].TC_IER = TC_IER_CPCS;   // IER = interrupt enable register
+	TC0->TC_CHANNEL[1].TC_IDR = ~TC_IER_CPCS;  // IDR = interrupt disable register
 											   // enable timer interrupts on the timer 
-
 	//Enable the interrupt in the nested vector interrupt controller
-  //TC3_IRQn where 3 is the timer number * timer channels (3) + the channel number (=(1*3)+0) for timer1 channel0
+	//TC3_IRQn where 3 is the timer number * timer channels (3) + the channel number (=(1*3)+0) for timer1 channel0
 	NVIC_EnableIRQ(TC3_IRQn);
-
+	//TC1_IRQn where 1 is the timer number * timer channels (3) + the channel number (=(0*3)+1) for timer0 channel1
+	NVIC_EnableIRQ(TC1_IRQn);
 	//Write the DACs
 	//dacc_set_channel_selection(DACC_INTERFACE, 0);       //select DAC channel 0
 	//dacc_write_conversion_data(DACC_INTERFACE, out_DAC0);//write on DAC
 	//dacc_set_channel_selection(DACC_INTERFACE, 1);       //select DAC channel 1
 	//dacc_write_conversion_data(DACC_INTERFACE, out_DAC1);//write on DAC
 
-	TC_Start(TC1, 0);
-
-
+	//TC_Start(TC1, 0);
+	TC_Start(TC0, 1);
 
 	Serial.begin(115200);
 	Serial.println("hello, making sine wave pulses");
@@ -56,34 +65,44 @@ void loop() {
 
 }
 
-// MAKE SMALLER INTS SO WE CAN STORE MORE!
-
 
 void TC3_Handler() //this is the ISR for the 500kHz timer - runs every 2 uS 
 {
 	// We need to get the status to clear it and allow the interrupt to fire again
-	TC_GetStatus(TC1, 0); //here TC1,0 means TIMER 1 channel 0
+	TC_GetStatus(TC1, 0); //here TC2,1 means TIMER 2 channel 1
 
-	digitalWriteDirect(41, 1);
+	//digitalWriteDirect(41, 1);
 
-	//Serial.print("1");
-
-	//dacout = waveformsTable[iSample];
-	//dacc_set_channel_selection(DACC_INTERFACE, 1);       //select DAC channel 1
 	dacc_write_conversion_data(DACC_INTERFACE, waveformsTable[iSample]);//write on DAC DACC_INTERFACE is currently *already* set to DAC1 from the original AnalogWrite call
-	//analogWrite(DAC1, dacout);
 
 	iSample++;
 
+	// if we have finished wavetable
 	if (iSample == maxSamplesNum)
 	{
-		iSample = 0;
+		iSample = 0; //reset counter
+		TC_Stop(TC1, 0); //stop this ISR
 	}
 
+
+	//digitalWriteDirect(41, 0);
+
+}
+
+void TC1_Handler() //this is the ISR for the 1Hz timer
+{
+	// We need to get the status to clear it and allow the interrupt to fire again
+	TC_GetStatus(TC0, 1); //here TC2,1 means TIMER 2 channel 1
+
+	digitalWriteDirect(41, 1);
+
+	TC_Start(TC1, 0); //start the DAC timer ISR
 
 	digitalWriteDirect(41, 0);
 
 }
+
+
 
 //taken from http://forum.arduino.cc/index.php?topic=129868.15
 inline void digitalWriteDirect(int pin, int val) {
